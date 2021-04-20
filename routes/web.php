7827df997723b8027ac59f658ad41cc62e826827
= <=>
2021-04-20 15:32:04 +0700

    dashboard main

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('home_mhs', [App\Http\Controllers\MhsController::class, 'home_mhs'])->name('master');
